4a2d41c0dec3de9e8572c3cd5faee489d1b0db5b
arsen <arsen.m@ucraft.com>
2022-12-14 17:48:42 +0400

    make transport identifier protected

--- src/Transports/ElasticTransport.php
+++ src/Transports/ElasticTransport.php
@@ -11,7 +11,7 @@ class ElasticTransport implements RecordDeliveryTransportInterface
     /**
      * @var string
      */
-    private string $identifier = 'elastic';
+    protected string $identifier = 'elastic';
 
     /**
      * @var array
